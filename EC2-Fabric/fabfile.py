7b2999ce6c76942e09abf7f5cacde5ba738ade81
Shivaji Vidhale <shivajividhale@Shivajis-MacBook-Pro.local>
2016-10-22 14:59:43 -0700

    Made it uniform for all users to run pem file. have your pem file in the home directory for this script to work.

--- EC2-Fabric/fabfile.py
+++ EC2-Fabric/fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import *
 import logging
 env.hosts = ['ec2-52-90-48-240.compute-1.amazonaws.com']
 env.user = 'ubuntu'
-env.key_filename = '/Users/shivajividhale/scalityHack/.ssh/s3f1.pem'
+env.key_filename = '~/s3f1.pem'
 
 # remote_uname()
 def local_uname():
