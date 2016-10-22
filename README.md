8f5d4531a4d04d0d43fbdf63ac397e1cb84b8728
Nicolas Humbert <nicolas.humbert@live.fr>
2016-10-22 00:06:06 -0700

    basic s3blaster config

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # S3F1
+
+
+## Start
+
+`npm start`
+
+Note: Do not forget to start S3.
+http://github.com/scality/s3blaster
+
+`S3BACKEND=mem npm start`
+
+## Collaborate
+
+Fork this repo using: https://help.github.com/articles/fork-a-repo/
+
+
+## S3blaster information
+
+### repo
+https://github.com/scality/s3blaster
+
+### Configuration
+https://github.com/scality/s3blaster/blob/master/CONFIGURATION.md
+
+
+## Questions
+
+How we gonna get/work with the output data?
+Modifying s3blaster code a bit? ...
---
+++ index.js
@@ -0,0 +1,38 @@
+const runS3Blaster = require('s3blaster').RunS3Blaster;
+
+const outputDir = `${__dirname}/results`;
+const params = {
+    host: 'localhost',
+    port: 8000,
+    forksNb: 1,
+    bucketsNb: 1,
+    bucketPrefix: 'bkts3std',
+    objectsNb: 2000,
+    sizes: [1],
+    unit: 'KB',
+    requests: 'put,get,delete',
+    range: ['0:1000', '1000:2000', '0:1000'],
+    fillObjs: true,
+    fillRange: '1000:2000',
+    fillThreads: 64,
+    paralReqs: [32, 64, 128, 256, 512],
+    schedule: 'each',
+    simulDelay: 5,
+    nextKey: 'seq',
+    observationsNb: 1e6,
+    workOnCurrObjs: false,
+    runTime: 10,
+    dontCleanDB: false,
+    ssm: false,
+    dirPath: outputDir,
+    output: 's3standard',
+    message: 'Performance measurement of S3 Server',
+};
+
+
+process.nextTick(runS3Blaster.start, params, err => {
+    if (err) {
+        console.log('err, s3f1 is not cool', err);
+    }
+    console.log('Wahouuu');
+});
