fdfc905bb3db230bc8b3e43a3410421613db2cb4
Bennett Buchanan <bennett.buchanan@scality.com>
2016-10-22 15:41:11 -0700

    Add fabfile to run S3 blaster on Scality S3

--- Local-Fabric/fabfile.py
+++ Local-Fabric/fabfile.py
@@ -1,1 +1,10 @@
-#Paste your fabfile here
+from fabric.api import *
+
+local('docker run -d -p 8000:8000 scality/s3server:mem-latest')
+
+# Set a delay for now to ensure the docker image is running
+local('sleep 20')
+local('npm start')
+
+# If restart is needed, run:
+# `docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)`
