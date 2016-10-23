34adb6796803d1a8d7a48a595d30c5c3f19d8e93
Bennett Buchanan <bennett.buchanan@scality.com>
2016-10-23 09:09:53 -0700

    Add more verbosity to fabfile

--- Local-Fabric/fabfile.py
+++ Local-Fabric/fabfile.py
@@ -1,9 +1,8 @@
 from fabric.api import *
 
 def deploy_local():
-    local('docker run -d -p 8000:8000 scality/s3server:mem-latest')
-
-    # Set a delay for now to ensure the docker image is running
+    local('docker run -d -p 8000:8000 scality/s3server:mem-latest')    
+    local('echo "Waiting for docker container..."')
     local('sleep 20')
     local('ACCESSKEYID=your_access_key_id SECRETACCESSKEY=your_secret_access_key')
     local('npm start')
