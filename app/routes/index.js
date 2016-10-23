8bc11358b579b51b12fd1699a63c86bbabfc0fe9
Bennett Buchanan <bennett.buchanan@scality.com>
2016-10-23 08:23:45 -0700

    Restyle code, move results.json to project root

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
-const results = require('../results.json');
+const results = require('../../results.json');
 const exec = require('child_process').exec;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
     res.render('index', { title: 'S3F1',
                           results: JSON.stringify(results, null, 2),
-                          parallelReq: JSON.parse(results[1].numParallelReq).toString(),
-                          latencyTime: JSON.parse(results[1].latency).toString()+"px",
+                          parallelReq: JSON.parse(
+                              results[1].numParallelReq).toString(),
+                          latencyTime: JSON.parse(
+                              results[1].latency).toString() + 'px',
                         });
 });
 
-router.get('/blast', function(req, res, next) {
+router.get('/blast', (req, res) => {
     exec('fab -f ./Local-Fabric/fabfile.py deploy_local',
-        function (error, stdout, stderr) {
+        (error, stdout) => {
             console.log('stdout: ' + stdout);
             if (error !== null) {
                  console.log('exec error: ' + error);
@@ -24,8 +26,10 @@ router.get('/blast', function(req, res, next) {
 
     res.render('index', { title: 'S3F1',
                           results: JSON.stringify(results, null, 2),
-                          parallelReq: JSON.parse(results[1].numParallelReq).toString(),
-                          latencyTime: JSON.parse(results[1].latency).toString()+"px",
+                          parallelReq: JSON.parse(
+                              results[1].numParallelReq).toString(),
+                          latencyTime: JSON.parse(
+                              results[1].latency).toString() + 'px',
                         });
 });
 
