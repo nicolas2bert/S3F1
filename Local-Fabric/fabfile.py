8593a745838268170eef0ef665726a225d51343c
Nicolas Humbert <nicolas.humbert@live.fr>
2016-10-23 11:43:20 -0700

    rework on localhost part

--- Local-Fabric/fabfile.py
+++ Local-Fabric/fabfile.py
@@ -3,10 +3,10 @@ from fabric.api import *
 def deploy_local():
     local('echo "Waiting 20 seconds for docker container..."')
     local('docker run -d -p 8000:8000 scality/s3server:mem-latest')
-    local('ACCESSKEYID=your_access_key_id SECRETACCESSKEY=your_secret_access_key')
+    # local('ACCESSKEYID=your_access_key_id SECRETACCESSKEY=your_secret_access_key')
 
 def run_blaster():
-    local('npm start')
+    local('ACCESSKEYID=accessKey1 SECRETACCESSKEY=verySecretKey1 npm start')
 
 # If restart is needed, run:
 # `docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)`
