6a3466e35acca317e491dd0ac7edfc9a5e3c3567
Bennett Buchanan <bennett.buchanan@scality.com>
2016-10-23 08:49:17 -0700

    Add blast.jade for route, abstract layout logic

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,13 +6,7 @@ const exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', { title: 'S3F1',
-                          results: JSON.stringify(results, null, 2),
-                          parallelReq: JSON.parse(
-                              results[1].numParallelReq).toString(),
-                          latencyTime: JSON.parse(
-                              results[1].latency).toString() + 'px',
-                        });
+    res.render('index', { title: 'S3F1' });
 });
 
 router.get('/blast', (req, res) => {
@@ -24,7 +18,7 @@ router.get('/blast', (req, res) => {
             }
         });
 
-    res.render('index', { title: 'S3F1',
+    res.render('blast', { title: 'S3F1',
                           results: JSON.stringify(results, null, 2),
                           parallelReq: JSON.parse(
                               results[1].numParallelReq).toString(),
