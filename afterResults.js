7d245287708cc31965b14a01c257896faaa35f1b
Dora Korpar <dora.korpar@scality.com>
2016-10-22 19:03:25 -0700

    function parses results of s3blaster

---
+++ afterResults.js
@@ -0,0 +1,50 @@
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+
+let fastestTimes = [
+	{ 'numParallelReq': 32,
+		'latency': 0 },
+	{ 'numParallelReq': 64,
+		'latency': 0 },
+	{ 'numParallelReq': 128,
+		'latency': 0 },
+	{ 'numParallelReq': 256,
+		'latency': 0 },
+	{ 'numParallelReq': 512, 
+		'latency': 0 }
+];
+const rl = readline.createInterface({
+	input: fs.createReadStream('./results/hello/scality/worker1/s3standard_stats.txt'),
+});
+
+function parseLines() {
+	let lineNumber = 0;
+	// console.log(fastestTimes);
+	rl.on('line', line => {
+		lineNumber += 1;
+		if (lineNumber > 7 && lineNumber < 53) {
+			const parRequests = line.toString().split(/\s+/)[2];
+			const latencyTime = line.toString().split(/\s+/)[3];
+			let index = 0;
+			while (index < fastestTimes.length) {
+				if (fastestTimes[index].numParallelReq == parRequests) {
+					if (fastestTimes[index].latency === 0 || latencyTime < fastestTimes[index].latency) {
+						fastestTimes[index].latency = latencyTime;
+					}
+				}
+				index += 1;
+			}
+		}
+	});
+	rl.on('close', () => {
+		fs.writeFile('./results.json', JSON.stringify(fastestTimes), err => {
+			if (err) {
+				return console.log(err);
+			}
+			return true;
+		});
+	});
+}
+
+parseLines();
